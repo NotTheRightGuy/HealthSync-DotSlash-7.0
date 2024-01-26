82ddfefdaa240b0bebf7b3fcfcadf508ba43dcd2
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-26 22:57:50 +0530

    Initial Boilerplate

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+                bricolage: ["Bricolage Grotesque", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
