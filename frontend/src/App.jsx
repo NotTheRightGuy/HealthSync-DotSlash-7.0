499a30941762b4157ebb40d3b90e6a63a4a6782e
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-01-27 18:37:34 +0530

    chakra wrapped

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,27 @@ import Diagnosis from "./pages/diagnosis"
 import Prescriptions from "./pages/prescriptions"
 import YourDoctor from "./pages/yourDoctor"
 import AIChatbot from "./pages/AIChatbot"
+import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
 
   return (
-    <div className="bg-black text-white h-screen">
-        <Router>
-            <Routes>
-                {/* <Route path="/" element={<Home/>}  ></Route> */}
-                {/* <Route path="/features" element={<Features/>}  ></Route> */}
-                <Route path="/patientDashboard" element={<PatientDashboard/>} ></Route>
-                <Route path="/patientDashboard/diagnosis" element={<Diagnosis/>} ></Route>
-                <Route path="/patientDashboard/prescriptions" element={<Prescriptions/>} ></Route>
-                <Route path="/patientDashboard/yourDoctor" element={<YourDoctor/>} ></Route>
-                <Route path="/patientDashboard/chatbot" element={<AIChatbot/>} ></Route>
-            </Routes>
-        </Router>   
-    </div>
+    <ChakraProvider>
+        <div className="bg-black text-white h-screen">
+            <Router>
+                <Routes>
+                    {/* <Route path="/" element={<Home/>}  ></Route> */}
+                    {/* <Route path="/features" element={<Features/>}  ></Route> */}
+                    <Route path="/patientDashboard" element={<PatientDashboard/>} ></Route>
+                    <Route path="/patientDashboard/diagnosis" element={<Diagnosis/>} ></Route>
+                    <Route path="/patientDashboard/prescriptions" element={<Prescriptions/>} ></Route>
+                    <Route path="/patientDashboard/yourDoctor" element={<YourDoctor/>} ></Route>
+                    <Route path="/patientDashboard/chatbot" element={<AIChatbot/>} ></Route>
+                </Routes>
+            </Router>   
+        </div>
+    </ChakraProvider>
+
     )
 }
 
