69b11d2254f3a0b498adaa1ed152869ec3a84f25
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-02-18 17:05:24 +0530

    Added Favicon

--- frontend/src/URL.js
+++ frontend/src/URL.js
@@ -1,5 +1,5 @@
-const REMOTE_URL = "ec2-52-66-237-98.ap-south-1.compute.amazonaws.com";
+const REMOTE_URL = "http://ec2-15-207-98-5.ap-south-1.compute.amazonaws.com";
 const LOCAL_URL = "http://localhost";
-const useLocal = true;
+const useLocal = false;
 const URL = useLocal ? LOCAL_URL : REMOTE_URL;
 export default URL;
