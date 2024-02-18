ceef695f896556c1b30deda6782e5ec5d8e2b536
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-02-18 15:36:57 +0530

    Changed Font

--- ml/utils/template_maker.py
+++ ml/utils/template_maker.py
@@ -13,24 +13,26 @@ def create_template(name, age, phone, blood_group, diagnosis, confidence_level, 
     confidence_level_coords = (381, 885)
     feedback_coords = (134, 1034)
 
+    # Assume linux oS with font DejaVuSans.ttf installed
+    font = ImageFont.truetype("DejaVuSans.ttf", 30)
     img = Image.open(prescription_template)
     draw = ImageDraw.Draw(img)
-    draw.text(date_coords, str(datetime.date.today()), (0, 0, 0))
-    draw.text(name_coords, name, (0, 0, 0))
-    draw.text(age_coords, str(age), (0, 0, 0))
-    draw.text(phone_coords, phone, (0, 0, 0))
-    draw.text(blood_group_coords, blood_group, (0, 0, 0))
-    draw.text(diagnosis_coords, diagnosis, (0, 0, 0))
-    draw.text(confidence_level_coords, confidence_level + "%", (0, 0, 0))
+    draw.text(date_coords, str(datetime.date.today()), (0, 0, 0), font=font)
+    draw.text(name_coords, name, (0, 0, 0), font=font)
+    draw.text(age_coords, str(age), (0, 0, 0), font=font)
+    draw.text(phone_coords, phone, (0, 0, 0), font=font)
+    draw.text(blood_group_coords, blood_group, (0, 0, 0), font=font)
+    draw.text(diagnosis_coords, diagnosis, (0, 0, 0), font=font)
+    draw.text(confidence_level_coords, confidence_level + "%", (0, 0, 0), font=font)
 
     y = 1034
     for feed in feedback:
-        draw.text((134, y), feed, (0, 0, 0))
+        draw.text((134, y), feed, (0, 0, 0), font=font)
         y += 50
 
     # Map through the medicine which will be a list of medicines and print them on the prescription
     y = 1619
     for med in medicine:
-        draw.text((155, y), med, (0, 0, 0))
+        draw.text((155, y), med, (0, 0, 0), font=font)
         y += 50
     img.save(f"dist/prescription-output.jpg")
