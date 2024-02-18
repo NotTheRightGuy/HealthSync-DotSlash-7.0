995529f3137dd97d03000737f3114fcbf18c1c05
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-02-19 00:57:36 +0530

    Trying somthing

--- frontend/src/URL.js
+++ frontend/src/URL.js
@@ -1,4 +1,4 @@
-const REMOTE_URL = "https://ec2-15-207-98-5.ap-south-1.compute.amazonaws.com";
+const REMOTE_URL = "https://node.atjam.study";
 const LOCAL_URL = "http://localhost";
 const useLocal = false;
 const URL = useLocal ? LOCAL_URL : REMOTE_URL;
