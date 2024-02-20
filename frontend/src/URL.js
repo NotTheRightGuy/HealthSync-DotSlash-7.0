e354ed44205823b07aaa521a51c6a4da254b36dd
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-02-20 14:10:10 +0530

    Fixed Typo on URL

--- frontend/src/URL.js
+++ frontend/src/URL.js
@@ -1,4 +1,4 @@
-const NODE_URL = "https://api.healthsync.life";
+const NODE_URL = "https://api.healthysync.life";
 const LOCAL_URL = "http://localhost";
 const useLocal = false;
 const URL = useLocal ? LOCAL_URL : NODE_URL;
